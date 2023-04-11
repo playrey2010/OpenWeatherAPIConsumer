1fc35adc8757174c7071657c848e867c22716fa6
playrey2010 <reynaldo.emanuel@gmail.com>
2023-04-10 17:10:51 -0700

    working first commit

---
+++ src/main/java/com/example/demo/controllers/WeatherController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.WeatherResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class WeatherController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @PostMapping("/weather")
+    public String showWeather(String city, Model model) {
+        String apiKey = "d107e64a83935fb95aafe7864d6e1952";
+        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+
+        RestTemplate restTemplate = new RestTemplate();
+        WeatherResponse weatherResponse = restTemplate.getForObject(url, WeatherResponse.class);
+
+        model.addAttribute("weather", weatherResponse);
+        model.addAttribute("city", city);
+
+        return "weather";
+    }
+}
---
+++ src/main/java/com/example/demo/models/WeatherResponse.java
@@ -0,0 +1,39 @@
+package com.example.demo.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class WeatherResponse {
+
+    private Main main;
+    private String name;
+
+    public Main getMain() {
+        return main;
+    }
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Main {
+        private double temp;
+
+        public double getTemp() {
+            return temp;
+        }
+
+        public void setTemp(double temp) {
+            this.temp = temp;
+        }
+    }
+}
